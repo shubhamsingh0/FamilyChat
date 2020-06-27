c0b47e26f849900e540e7dc2b9f2dae80b37a546
Shubham Singh <shubham2singh0@gmail.com>
2020-06-27 16:57:50 +0530

    Initial commit

---
+++ src/main/java/com/applications/familychat/FamilyChatApplication.java
@@ -0,0 +1,13 @@
+package com.applications.familychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applications/familychat/FamilyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applications.familychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
